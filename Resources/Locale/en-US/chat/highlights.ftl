space-wizards/space-station-14

Improvements to automatic job highlights (#42630)
* Improvements to automatic chat highlights

* Automatic highlights option is more clearly labelled

Resources/Locale/en-US/chat/highlights.ftl
@@ -2,54 +2,55 @@
 highlights-captain = Captain, "Cap", Bridge, Command
 highlights-head-of-personnel = Head Of Personnel, "HoP", Service, Bridge, Command
 highlights-chief-engineer = Chief Engineer, "CE", Engineering, Engineer, "Engi", Bridge, Command
-highlights-chief-medical-officer = Chief Medical Officer, "CMO", MedBay, "Med", Bridge, Command
-highlights-head-of-security = Head of Security, "HoS", Security, "Sec", Bridge, Command
-highlights-quartermaster = Quartermaster, "QM", Cargo, Bridge, Command
-highlights-research-director = Research Director, "RD", Science, "Sci", Bridge, Command
+highlights-chief-medical-officer = Chief Medical Officer, "CMO", Medbay, Medical, "Med", Bridge, Command
+highlights-head-of-security = Head of Security, "HoS", Armory, Security, "Sec", Bridge, Command
+highlights-quartermaster = Quartermaster, "QM", Cargo, Supply, Bridge, Command
+highlights-research-director = Research Director, "RD", Science, "Sci", "RND", "R&D", Bridge, Command
 
 # Security
-highlights-detective = Detective, "Det", Security, "Sec"
-highlights-security-cadet = Security Cadet, Secoff, Cadet, Security, "Sec"
-highlights-security-officer = Security Officer, Secoff, Officer, Security, "Sec"
-highlights-warden = Warden, "Ward", Security, "Sec"
+highlights-detective = Detective, "Det", Armory, Security, "Sec"
+highlights-security-cadet = Security Cadet, Secoff, Cadet, Armory, Security, "Sec"
+highlights-security-officer = Security Officer, Secoff, Officer, Armory, Security, "Sec"
+highlights-warden = Warden, "Ward", Brig, Genpop, Jail, "Prison", Armory, Security, "Sec"
 
 # Cargo
-highlights-cargo-technician = Cargo Technician, Cargo Tech, "Cargo"
-highlights-salvage-specialist = Salvage Specialist, Salvager, Salvage, "Salv", "Cargo", Miner
+highlights-cargo-technician = Cargo Technician, Cargo Tech, "Cargo", Supply
+highlights-salvage-specialist = Salvage Specialist, Salvager, Salvage, "Salv", Miner, "Cargo", Supply
 
 # Engineering
-highlights-atmospheric-technician = Atmospheric Technician, Atmos tech, Atmospheric, Engineering, "Atmos", "Engi"
+highlights-atmospheric-technician = Atmospheric Technician, Atmos Tech, Atmospheric, Engineering, "Atmos", "Engi"
 highlights-station-engineer = Station Engineer, Engineering, Engineer, "Engi"
 highlights-technical-assistant = Technical Assistant, Tech Assistant, Engineering, Engineer, "Engi"
 
 # Medical
-highlights-chemist = Chemist, Chemistry, "Chem", MedBay, "Med"
-highlights-medical-doctor = Medical Doctor, Doctor, "Doc", MedBay, "Med"
-highlights-medical-intern = Medical Intern, "Doc", Intern, MedBay, "Med"
-highlights-paramedic = Paramedic, "Para", MedBay, "Med"
+highlights-chemist = Chemist, Chemistry, "Chem", Medbay, Medical, "Med"
+highlights-medical-doctor = Medical Doctor, Doctor, "Doc", Medbay, Medical, "Med"
+highlights-medical-intern = Medical Intern, Intern, Medbay, Medical, "Med"
+highlights-paramedic = Paramedic, "Para", "Medic", Medbay, Medical, "Med"
 
 # Science
-highlights-scientist = Scientist, Science, "Sci"
-highlights-research-assistant = Research Assistant, Science, "Sci"
+highlights-scientist = Scientist, Science, "Sci", "RND", "R&D"
+highlights-research-assistant = Research Assistant, Science, "Sci", "RND", "R&D"
 
 # Civilian
-highlights-bartender = Bartender, Barkeeper, Barkeep, "Bar"
-highlights-botanist = Botanist, Botany, Hydroponics
-highlights-chaplain = Chaplain, "Chap", Chapel
-highlights-chef = Chef, "Cook", Kitchen
-highlights-clown = Clown, Jester
-highlights-janitor = Janitor, "Jani"
-highlights-lawyer = Lawyer, Attorney
-highlights-librarian = Librarian, Library
-highlights-mime = Mime
-highlights-passenger = Passenger, Greytider, "Tider"
-highlights-service-worker = Service Worker
+highlights-bartender = Bartender, Barkeeper, Barkeep, "Bar", Service, "Serv"
+highlights-botanist = Botanist, Botany, Hydroponics, Service, "Serv"
+highlights-chaplain = Chaplain, "Chap", Chapel, Service, "Serv"
+highlights-chef = Chef, "Cook", Kitchen, Service, "Serv"
+highlights-clown = Clown, Theatre, Theater, Service, "Serv"
+highlights-janitor = Janitor, "Jani", Service, "Serv"
+highlights-lawyer = Lawyer, Attorney, "Law", Service, "Serv"
+highlights-librarian = Librarian, Library, Service, "Serv"
+highlights-mime = Mime, Theatre, Theater, Service, "Serv"
+highlights-musician = Musician, "Music", Theatre, Theater, Service, "Serv"
+highlights-passenger = Passenger, Greytider, Graytider, "Tider", "Tide"
+highlights-service-worker = Service Worker, Service, "Serv"
 
 # Station-specific
-highlights-reporter = Reporter, Journalist
-highlights-psychologist = Psychologist, Psychology
+highlights-reporter = Reporter, Journalist, Newsroom, News
+highlights-psychologist = Psychologist, Psychology, "Psych", Medbay, Medical, "Med"
 
 # Silicon
 highlights-personal-ai = Personal AI, "pAI"
-highlights-cyborg = Cyborg, Silicon, Borg
+highlights-cyborg = Cyborg, Silicon, Borg, Robotics, "Robot"
 highlights-station-ai = Station AI, Silicon, "AI", "sAI"

IAmNotGray/Delta-v

Resources/Locale/en-US/chat/highlights.ftl
@@ -2,55 +2,54 @@
 highlights-captain = Captain, "Cap", Bridge, Command
 highlights-head-of-personnel = Head Of Personnel, "HoP", Service, Bridge, Command
 highlights-chief-engineer = Chief Engineer, "CE", Engineering, Engineer, "Engi", Bridge, Command
-highlights-chief-medical-officer = Chief Medical Officer, "CMO", MedBay, "Med", Bridge, Command
-highlights-head-of-security = Head of Security, "HoS", Security, "Sec", Bridge, Command
-highlights-quartermaster = Quartermaster, "QM", Cargo, "Logi", Logistics, Bridge, Command
+highlights-chief-medical-officer = Chief Medical Officer, "CMO", MedBay, Medical, "Med", Bridge, Command
+highlights-head-of-security = Head of Security, "HoS", Security, Armory, "Sec", Bridge, Command
+highlights-quartermaster = Quartermaster, "QM", Cargo, "Logi", Logistics, Supply, Bridge, Command
 highlights-research-director = Research Director, "RD", Science, "Sci", Bridge, Command
 
 # Security
-highlights-detective = Detective, "Det", Security, "Sec"
-highlights-security-cadet = Security Cadet, Secoff, Cadet, Security, "Sec"
-highlights-security-officer = Security Officer, Secoff, Officer, Security, "Sec"
-highlights-warden = Warden, "Ward", Security, "Sec"
+highlights-detective = Detective, "Det", Armory, Security, "Sec"
+highlights-security-cadet = Security Cadet, Secoff, Cadet, Armory, Security, "Sec"
+highlights-security-officer = Security Officer, Secoff, Officer, Armory, Security, "Sec"
+highlights-warden = Warden, "Ward", Brig, Genpop, Jail, "Prison", Armory, Security, "Sec"
 
 # Cargo
 highlights-cargo-technician = Cargo Technician, Cargo Tech, "Cargo", "Logi", Logistics
 highlights-salvage-specialist = Salvage Specialist, Salvager, Salvage, "Salv", "Cargo", Miner, "Logi", Logistics
 
 # Engineering
-highlights-atmospheric-technician = Atmospheric Technician, Atmos tech, Atmospheric, Engineering, "Atmos", "Engi"
+highlights-atmospheric-technician = Atmospheric Technician, Atmos Tech, Atmospheric, Engineering, "Atmos", "Engi"
 highlights-station-engineer = Station Engineer, Engineering, Engineer, "Engi"
 highlights-technical-assistant = Technical Assistant, Tech Assistant, Engineering, Engineer, "Engi"
 
 # Medical
-highlights-chemist = Chemist, Chemistry, "Chem", MedBay, "Med"
-highlights-medical-doctor = Medical Doctor, Doctor, "Doc", MedBay, "Med"
-highlights-medical-intern = Medical Intern, "Doc", Intern, MedBay, "Med"
-highlights-paramedic = Paramedic, "Para", MedBay, "Med"
+highlights-chemist = Chemist, Chemistry, "Chem", Medbay, Medical, "Med"
+highlights-medical-doctor = Medical Doctor, Doctor, "Doc", Medbay, Medical, "Med"
+highlights-medical-intern = Medical Intern, Intern, Medbay, Medical, "Med"
+highlights-paramedic = Paramedic, "Para", "Medic", Medbay, Medical, "Med"
 
 # Science
 highlights-scientist = Scientist, Science, "Sci", Epistemics, "Epi"
 highlights-research-assistant = Research Assistant, Science, "Sci", Epistemics, "Epi"
 
 # Civilian
-highlights-bartender = Bartender, Barkeeper, Barkeep, "Bar"
-highlights-botanist = Botanist, Botany, Hydroponics
+highlights-bartender = Bartender, Barkeeper, Barkeep, "Bar", Service, "Serv"
+highlights-botanist = Botanist, Botany, Hydroponics, Service, "Serv"
 highlights-chaplain = Chaplain, "Chap", Chapel, Science, "Sci", Epistemics, "Epi"
-highlights-chef = Chef, "Cook", Kitchen
-highlights-clown = Clown, Jester
-highlights-janitor = Janitor, "Jani"
-highlights-lawyer = Lawyer, Attorney
+highlights-chef = Chef, "Cook", Kitchen, Service, "Serv"
+highlights-clown = Clown, Jester, Service, "Serv"
+highlights-janitor = Janitor, "Jani", Service, "Serv"
+highlights-lawyer = Lawyer, Attorney, "Law", Service, "Serv"
 highlights-librarian = Librarian, Library
-highlights-mime = Mime
-highlights-passenger = Passenger, Greytider, "Tider"
-highlights-service-worker = Service Worker
+highlights-mime = Mime, Service, "Serv"
+highlights-passenger = Passenger, Greytider, Graytider, "Tider", "Tide"
+highlights-service-worker = Service Worker, Service, "Serv"
 
 # Station-specific
-highlights-reporter = Reporter, Journalist
-highlights-psychologist = Psychologist, Psychology, "Psych"
+highlights-reporter = Reporter, Journalist, Newsroom, News
+highlights-psychologist = Psychologist, Psychology, "Psych", Medbay, Medical, "Med"
 
 # Silicon
 highlights-personal-ai = Personal AI, "pAI"
-highlights-cyborg = Cyborg, Silicon, Borg
+highlights-cyborg = Cyborg, Silicon, Borg, Robotics, "Robot"
 highlights-station-ai = Station AI, Silicon, "AI", "sAI"
-