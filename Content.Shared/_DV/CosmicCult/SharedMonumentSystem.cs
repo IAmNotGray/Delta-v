10a3756edbfd774d1496fe896269637aad968ea4
Janet Blackquill <uhhadd@gmail.com>
2026-07-03 02:22:16 -0400

    get glyphed idiot

--- Content.Shared/_DV/CosmicCult/SharedMonumentSystem.cs
+++ Content.Shared/_DV/CosmicCult/SharedMonumentSystem.cs
@@ -29,8 +29,6 @@ public abstract class SharedMonumentSystem : EntitySystem
     {
         base.Initialize();
         SubscribeLocalEvent<MonumentComponent, BoundUIOpenedEvent>(OnUIOpened);
-        SubscribeLocalEvent<MonumentComponent, GlyphSelectedMessage>(OnGlyphSelected);
-        SubscribeLocalEvent<MonumentComponent, GlyphRemovedMessage>(OnGlyphRemove);
         SubscribeLocalEvent<MonumentComponent, InfluenceSelectedMessage>(OnInfluenceSelected);
         SubscribeLocalEvent<MonumentOnDespawnComponent, TimedDespawnEvent>(OnTimedDespawn);
         SubscribeLocalEvent<MonumentCollisionComponent, PreventCollideEvent>(OnPreventCollide);
@@ -80,38 +78,6 @@ public abstract class SharedMonumentSystem : EntitySystem
     }
 
     #region UI listeners
-    private void OnGlyphSelected(Entity<MonumentComponent> ent, ref GlyphSelectedMessage args)
-    {
-        ent.Comp.SelectedGlyph = args.GlyphProtoId;
-
-        if (!_prototype.TryIndex(args.GlyphProtoId, out var proto))
-            return;
-
-        var xform = Transform(ent);
-
-        if (!TryComp<MapGridComponent>(xform.GridUid, out var grid))
-            return;
-
-        var localTile = _map.GetTileRef(xform.GridUid.Value, grid, xform.Coordinates);
-        var targetIndices = localTile.GridIndices + new Vector2i(0, -1);
-
-        if (ent.Comp.CurrentGlyph is { } curGlyph) _glyph.EraseGlyph(curGlyph);
-
-        var glyphEnt = Spawn(proto.Entity, _map.ToCenterCoordinates(xform.GridUid.Value, targetIndices, grid));
-        ent.Comp.CurrentGlyph = glyphEnt;
-        var evt = new CosmicCultAssociateRuleEvent(ent, glyphEnt);
-        RaiseLocalEvent(ref evt);
-
-        _ui.SetUiState(ent.Owner, MonumentKey.Key, new MonumentBuiState(ent.Comp));
-    }
-
-    private void OnGlyphRemove(Entity<MonumentComponent> ent, ref GlyphRemovedMessage args)
-    {
-        if (ent.Comp.CurrentGlyph is { } curGlyph) _glyph.EraseGlyph(curGlyph);
-
-        _ui.SetUiState(ent.Owner, MonumentKey.Key, new MonumentBuiState(ent.Comp));
-    }
-
     private void OnInfluenceSelected(Entity<MonumentComponent> ent, ref InfluenceSelectedMessage args)
     {
         if (!_prototype.TryIndex(args.InfluenceProtoId, out var proto) || !TryComp<ActivatableUIComponent>(ent, out var uiComp) || !TryComp<CosmicCultComponent>(args.Actor, out var cultComp))
